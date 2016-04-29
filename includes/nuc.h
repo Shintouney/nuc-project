3d7c94225dc32c0e032d4ed18fdde1cb2652a47c
Haitem Zeghouani <shinta42@hotmail.fr>
2016-04-29 17:03:44 +0200

    Last commit

--- includes/nuc.h
+++ includes/nuc.h
@@ -1,8 +1,15 @@
 #ifndef NUC_H
 # define NUC_H
 
+// Macro
+# define PORT_ACCESS (5050)
+# define ANSI_COLOR_GREEN "\x1b[32m"
+# define ANSI_COLOR_RESET "\x1b[0m"
+
 // Includes
 // Not here right now
+#include <stdio.h>
+#include <sys/socket.h>
 
 // Prototype functions
 void  my_putchar(char c);
--- srcs/main.c
+++ srcs/main.c
@@ -2,6 +2,23 @@
 
 int       main(int argc, const char *argv[])
 {
-  my_putstr("Init program\n");
+  int     init;
+  int     tmp;
+
+  tmp  = 0;
+  init = 0;
+
+  while (init != 3)
+  {
+    if (tmp == 0)
+    {
+      my_putstr(ANSI_COLOR_GREEN);
+      my_putstr("Init program\n");
+      my_putstr("Socket send to broadcast addresse : 0.0.0.0\n");
+      my_putstr("wating for response ...");
+      my_putstr(ANSI_COLOR_RESET);
+      tmp++;
+    }
+  }
   return (0);
 }
